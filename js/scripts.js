ef26684a9d2b266360c848c6360f23f832729b99
Ben White <bwhite2140@outlook.com>
2020-06-15 15:54:38 -0700

    add user logic for spec 4

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,18 @@ ToDoList.prototype.addToList = function(task) {
   this.tasks.push(task);
 }
 
+ToDoList.prototype.removeFromList = function(remove) {
+  for (let i = 0; i < this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (remove) {
+        delete this.tasks[i];
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function Task(chore) {
   this.chore = chore;
 }
@@ -21,9 +33,24 @@ $(document).ready(function() {
     let toDoList = new ToDoList();
     const chore = new Task($("input#task").val());
     toDoList.addToList(chore);
+    console.log(toDoList)
 
     $("#output").show();
-    $("ul").append("<li>" + "<input type='radio' name='done' value='yes'> " + chore.display() + "</li>");
+    $("ul").append("<li>" + "<input type='checkbox' name='done' value='yes'> " + chore.display() + "</li>");
+    $("#done").show();
+    $("#done").submit(function(event) {
+      event.preventDefault();
+      $("input:checkbox[name=done]:checked").each(function() {
+        const complete = $(this).val();
+        toDoList.removeFromList(complete);
+      });
+      $("li").remove();
+      for (let i = 0; i < toDoList.length; i++) {
+        if (toDoList[i]) {
+          $("ul").append(toDoList[i]);
+        }
+      }
+    });
     // $("#finished").show();
     // $("form#finished").submit(function(event) {
     //   event.preventDefault();
